440738fc4933ab5d044e11fb764e3c3ecaae683c
Kanishk Agrawal <kanishk120@outlook.com>
2020-09-14 11:08:15 +0000

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,6 +1,12 @@
 provider "azurerm" {
   features {}
- 
+   
+  backend "azurerm" {
+    resource_group_name  = "tf-storage-rg"
+    storage_account_name = "tfsa120"
+    container_name       = "tfstate"
+    key                  = "terraform.tfstate"
+  }
 }
 
 resource "azurerm_resource_group" "main" {
